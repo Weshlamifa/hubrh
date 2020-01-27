b6831885f38197177eb0e2fc6be47e123bc80f3f
Alexis LORET <a.loret@groupeonepoint.com>
2020-01-27 12:11:31 +0100

    Ajout des components header et collaborateurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { MatSliderModule } from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollaborateursComponent } from './collaborateurs/collaborateurs.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
+
+const appRoutes: Routes = [
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollaborateursComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     MatSliderModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule
   ],
   providers: [],
