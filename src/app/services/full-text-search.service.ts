cf2c8bf2d668106fd80e2c5557aa182f0bfa3821
Louis QUESTEL <l.questel@groupeonepoint.com>
2020-01-28 14:59:53 +0100

    upperCase

--- src/app/services/full-text-search.service.ts
+++ src/app/services/full-text-search.service.ts
@@ -5,13 +5,13 @@ import { Injectable, PipeTransform } from '@angular/core';
 export class FullTextSearchService {
 
  transform( sea: string, data: any): any {
-	 console.log(data);
-	return (typeof data !== 'undefined') ? data.filter((item) => {
-      return item.name.startsWith(sea) || item.firstname.startsWith(sea);
-	}) : true;
+  console.log(data);
+  return (typeof data !== 'undefined') ? data.filter((item) => {
+      return item.name.toUpperCase().startsWith(sea.toUpperCase()) || item.firstname.toUpperCase().startsWith(sea.toUpperCase());
+  }) : true;
  }
 
   transformSolo( data: any, sea: string): any {
-    return (typeof data !== 'undefined') ? data.filter((item) => item.name.startsWith(sea)) : true;
+    return (typeof data !== 'undefined') ? data.filter((item) => item.name.toUpperCase().startsWith(sea.toUpperCase())) : true;
   }
 }
