f89d1f3938712eaac6de7ecb906e1c9dfe25a437
Louis QUESTEL <l.questel@groupeonepoint.com>
2020-01-28 09:47:16 +0100

    wala mon frère

--- src/app/collaborateurs/collaborateurs.component.ts
+++ src/app/collaborateurs/collaborateurs.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Input,  OnInit, ViewChild } from '@angular/core';
+import { Component, Input,  OnInit, ViewChild, SystemJsNgModuleLoader } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { DataTableDataSource } from './data-table-datasource';
 @Component({
@@ -13,10 +13,22 @@ export class CollaborateursComponent implements OnInit {
   @ViewChild(MatSort, {static : true}) sort: MatSort;
   dataSource: DataTableDataSource;
 
+  //barre de recherche
+  data : string;
+
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['nom', 'prenom', 'email'];
 
   ngOnInit() {
     this.dataSource = new DataTableDataSource(this.paginator, this.sort);
+  }
+
+  change() {
+    // requete http sur la bdd
+    console.log(this.data);
+  }
+
+  research(test) {
+    console.log(test);
   }
 }
