fd86d7e43d87c9dd08922e67de3d20750fcfc5f8
Alexis LORET <a.loret@groupeonepoint.com>
2020-01-29 16:07:14 +0100

    Petits changements, checkbox par defaut qui renvoie false

--- src/app/modal-add-collaborateur/modal-add-collaborateur.component.ts
+++ src/app/modal-add-collaborateur/modal-add-collaborateur.component.ts
@@ -18,13 +18,15 @@ export class ModalAddCollaborateurComponent implements OnInit {
       contactFormModalEmail: new FormControl('', [Validators.required, Validators.email]),
       contactFormModalFirstName: new FormControl('', Validators.required),
       contactFormModalDate: new FormControl('', Validators.required),
-      contactFormModalPrestataire: new FormControl('', Validators.required),
+      contactFormModalPrestataire: new FormControl(false, Validators.required),
       contactFormModalRole: new FormControl('', Validators.required),
       contactFormModalStatut: new FormControl('', Validators.required),
       contactFormModalLinkCV: new FormControl(''),
       contactFormModalPhoto: new FormControl(''),
       contactFormModalComments: new FormControl('')
     });
+
+    
   }
 
   constructor(private collaborateurs: CollaborateursComponent) {this.prestataire = false;  }
@@ -35,17 +37,15 @@ export class ModalAddCollaborateurComponent implements OnInit {
       const firstname = this.contactFormModalFirstName.value;
       const email = this.contactFormModalEmail.value;
       const date = this.contactFormModalDate.value;
-      if(this.prestataire != null) {
-        this.prestataire = this.contactFormModalPrestataire.value;
-      }
+      this.prestataire = this.contactFormModalPrestataire.value;
+      this.prestataire = this.contactFormModalPrestataire.value;
       const role = this.contactFormModalRole.value;
       const statut = this.contactFormModalStatut.value;
       const linkcv = this.contactFormModalLinkCV.value;
       const photo = this.contactFormModalPhoto.value;
       const comments = this.contactFormModalComments.value;
       this.collaborateurs.add(name, firstname, email, date ,this.prestataire, role, statut, linkcv, comments);
-      this.validatingForm.reset();
-    
+      this.validatingForm.reset();   
   }
 
   onClose() {
