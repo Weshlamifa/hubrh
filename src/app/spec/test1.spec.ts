b1cf552065ea83f9f22933845c4e158a0e1dd56f
Alexis LORET <a.loret@groupeonepoint.com>
2020-02-07 13:32:40 +0100

    Ajout des tests

---
+++ src/app/spec/test1.spec.ts
@@ -0,0 +1,19 @@
+import { CollaborateursComponent } from '../collaborateurs/collaborateurs.component';
+     
+describe('someReducer', () => {
+     /*
+    it('should add a start parameter to true after ACTION.START', () => {
+ 
+        const result = someReducer({}, { type: ACTION.START });
+        expect(result).toEqual({ start: true });
+ 
+    });
+ 
+    it('should return the former state', () => {
+ 
+        const result = someReducer({}, { type: 'autre' });
+        expect(result).toEqual({});
+ 
+    });
+     */
+});
